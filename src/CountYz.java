65fb11368334dfd878e50197a60458a67d932bea
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-21 14:20:20 +0400

    if statement is written inside the loop

--- src/CountYz.java
+++ src/CountYz.java
@@ -7,7 +7,9 @@ public class CountYz {
         int y = str.indexOf("y");
 
         for (int i=0; i<str.length();i++){
-
+            if (str.charAt(i) == z && str.charAt(i+1)== ' ' ||
+                    str.charAt(i) == y && str.charAt(i+1)== ' '){
+                count++;
             }
         }
 
