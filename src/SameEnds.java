353a32e995b8f97c431788f04686c4c7fdeeace9
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-21 15:52:46 +0400

    2 string start and end are declared

--- src/SameEnds.java
+++ src/SameEnds.java
@@ -2,6 +2,11 @@ public class SameEnds {
     public static void main(String[] args){
         String str = "abXYab";
 
+        String start = str.substring(0,2);
+        String end = str.substring(str.length()-2,str.length());
+
+
+
 
     }
 }
