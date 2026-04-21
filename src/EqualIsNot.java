0a8f81318342d28ca947e2c75d7b122365e816fd
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-21 14:50:47 +0400

    if statement is written inside the loop to count both is and not

--- src/EqualIsNot.java
+++ src/EqualIsNot.java
@@ -1,13 +1,17 @@
 public class EqualIsNot {
     public static void main(String[] args){
-        String str = "This is not";
+        String str = "This is notnot";
 
         int countIs = 0;
         int countNot = 0;
 
         for (int i =0; i<str.length();i++){
-
-
+            if (str.charAt(i)== 'i' && str.charAt(i+1)== 's'){
+                countIs++;
+            }
+            if (str.charAt(i)== 'n' && str.charAt(i+1)== 'o' && str.charAt(i+2)== 't'){
+                countNot++;
+            }
         }
 
     }
