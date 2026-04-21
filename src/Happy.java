db1e80e673e298f5f9aa4080448fbeb179a82ab7
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-21 15:07:43 +0400

    the result is printed on the screen

--- src/Happy.java
+++ src/Happy.java
@@ -1,12 +1,17 @@
 public class Happy {
     public static void main(String[] args){
-        String str = "xxggxx";
+        String str = "xxggyygxx";
         boolean happy = true;
 
         if (happy ==true){
             for (int i =0; i<str.length();i++){
-
+                if (str.charAt(i)=='g'&& str.charAt(i+1)=='g'){
+                    System.out.println(true);
+                }
             }
+        }else {
+            happy=false;
+            System.out.println(happy);
         }
 
     }
