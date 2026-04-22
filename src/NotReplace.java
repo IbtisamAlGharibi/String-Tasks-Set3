d40337bff7670356812e95fd689e88fd5b16fe22
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-22 09:49:02 +0400

    if statement is written inside the loop

--- src/NotReplace.java
+++ src/NotReplace.java
@@ -2,9 +2,16 @@ public class NotReplace {
     public static void main(String[] args){
 
         String str= "is test";
+        String result = "";
 
         for (int i=0; i <str.length();i++){
             boolean check = Character.isLetter(str.charAt(i));
+
+            if (str.charAt(i) == 'i' && str.charAt(i+1) == 's'){
+               result = "is not" ;
+            }
+
+            result = result+ str.charAt(i);
 
         }
 
