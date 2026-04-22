c67a5d48b8188e4ef8798a11cc0330faa5062305
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-22 08:46:21 +0400

    string result is declared

--- src/MirrorEnds.java
+++ src/MirrorEnds.java
@@ -1,13 +1,14 @@
 public class MirrorEnds {
     public static void main(String[] args){
         String str = "abXYZba";
+        String result = "";
 
         for (int i =0; i<str.length();i++){
 
-
+        if (str.charAt(i) == str.charAt(str.length()-1-i)){
+            result = result+str.charAt(i);
         }
-
-
+        }
 
 
     }
