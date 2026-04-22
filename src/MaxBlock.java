58a49a3472c295445f7c2e86580b1d14f49772e7
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-22 09:06:18 +0400

    another if statement is written to compare the laregest value

--- src/MaxBlock.java
+++ src/MaxBlock.java
@@ -1,17 +1,22 @@
 public class MaxBlock {
     public static void main(String[] args){
-        String str = "abbCCCddBBBxx";
-        int count = 0;
-        int max =0;
 
-        for (int i =0; i<str.length();i++){
-            if (str.charAt(i) == str.charAt(i+1)){
+        String str = "abbCCCddBBBxx";
+        int count = 1;
+        int max = 1;
+
+        for (int i = 0; i < str.length() - 1; i++){
+
+            if (str.charAt(i) == str.charAt(i + 1)){
                 count++;
+            } else {
+                count = 1;
             }
 
+            if (count > max){
+                max = count;
+            }
         }
-
-
 
 
     }
