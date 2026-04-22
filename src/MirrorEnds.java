50779f4015f991c47be82c1a25db0a884b42414a
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-22 08:40:24 +0400

    for loop is created

--- src/MirrorEnds.java
+++ src/MirrorEnds.java
@@ -2,6 +2,13 @@ public class MirrorEnds {
     public static void main(String[] args){
         String str = "abXYZba";
 
+        for (int i =0; i<str.length();i++){
+
+
+        }
+
+
+
 
     }
 }
