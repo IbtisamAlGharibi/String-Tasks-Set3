2f5b2f7bda313a70db760caf0d07a5d70512dfdf
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-22 09:00:19 +0400

    if statement is written inside the loop

--- src/MaxBlock.java
+++ src/MaxBlock.java
@@ -5,6 +5,9 @@ public class MaxBlock {
         int max =0;
 
         for (int i =0; i<str.length();i++){
+            if (str.charAt(i) == str.charAt(i+1)){
+                count++;
+            }
 
         }
 
